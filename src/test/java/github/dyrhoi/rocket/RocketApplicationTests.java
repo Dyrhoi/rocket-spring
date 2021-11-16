7e44c2c4eadb2db34bde636ddd78915a7a25b28d
Dyrhoi <39205283+Dyrhoi@users.noreply.github.com>
2021-11-16 19:47:36 +0100

    Initial Commit

---
+++ src/test/java/github/dyrhoi/rocket/RocketApplicationTests.java
@@ -0,0 +1,13 @@
+package github.dyrhoi.rocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
